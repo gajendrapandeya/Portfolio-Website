17ae30ddb4786242bb730eccdae54443377f0921
gajendrapandeya <developerpandeya@gmail.com>
2022-02-02 00:08:40 +0545

    Created Project Section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 const collapsibles = document.querySelectorAll('.collapsible')
-collapsibles.forEach((item) => 
-item.addEventListener('click', function() {
-    this.classList.toggle('collapsible--expanded')
-})
+collapsibles.forEach((item) =>
+    item.addEventListener('click', function () {
+        this.classList.toggle('collapsible--expanded')
+    })
 )
+
+function scrollTo(to, duration) {
+    if (document.body.scrollTop == to) return;
+    var diff = to - document.body.scrollTop;
+    var scrollStep = Math.PI / (duration / 10);
+    var count = 0, currPos;
+    start = element.scrollTop;
+    scrollInterval = setInterval(function () {
+        if (document.body.scrollTop != to) {
+            count = count + 1;
+            currPos = start + diff * (0.5 - 0.5 * Math.cos(count * scrollStep));
+            document.body.scrollTop = currPos;
+        }
+        else { clearInterval(scrollInterval); }
+    }, 10);
+}
+
+function test(elID) {
+    var dest = document.getElementById(elID);
+    scrollTo(dest.offsetTop, 500);
+}
